55766890cbe77c3b62351f1cd8706699ef154ef5
Saul Vega <saulv@MacBook-Pro.local>
2020-09-17 00:47:12 -0400

    add event listener for hex copy

---
+++ creativeJavascriptCourse/coloors/app.js
@@ -0,0 +1,146 @@
+//global selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+const popup = document.querySelector('.copy-container');
+
+//event listeners
+sliders.forEach((slider) => {
+  slider.addEventListener('input', hslControls);
+});
+colorDivs.forEach((div, index) => {
+  div.addEventListener('change', () => {
+    updateTextUI(index);
+  });
+});
+currentHexes.forEach((hex) => {
+  hex.addEventListener('click', () => {
+    copyToClipboard(hex);
+  });
+});
+popup.addEventListener('transitionend', () => {
+  const popupBox = popup.children[0];
+  popup.classList.remove('active');
+  popupBox.classList.remove('active');
+});
+//functions
+
+//color generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColor() {
+  initialColors = [];
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+    //Add it to the array
+    if (div.classList.contains('locked')) {
+      initialColors.push(hexText.innerText);
+      return;
+    } else {
+      initialColors.push(chroma(randomColor).hex());
+    }
+
+    //Add the color to the bg
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+
+    //Check for contrast
+    checkTextContrast(randomColor, hexText);
+    //Initial Colorize Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll('.sliders input');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
+  });
+  //Reset Inputs
+  resetInputs();
+}
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = 'black';
+  } else {
+    text.style.color = 'white';
+  }
+}
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scale saturation
+  const noSat = color.set('hsl.s', 0);
+  const fullSat = color.set('hsl.s', 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //scale brightness
+  const midBright = color.set('hsl.l', 0.5);
+  const scaleBright = chroma.scale(['black', midBright, 'white']);
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)},
+  ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+    0
+  )},${scaleBright(0.5)},
+  ${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+function hslControls(e) {
+  const index =
+    e.target.getAttribute('data-bright') ||
+    e.target.getAttribute('data-sat') ||
+    e.target.getAttribute('data-hue');
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+  const bgColor = initialColors[index];
+  let color = chroma(bgColor)
+    .set('hsl.s', saturation.value)
+    .set('hsl.l', brightness.value)
+    .set('hsl.h', hue.value);
+  colorDivs[index].style.backgroundColor = color;
+
+  //colorize sliders
+  colorizeSliders(color, hue, brightness, saturation);
+}
+function updateTextUI(index) {
+  const activeDiv = colorDivs[index];
+  const color = chroma(activeDiv.style.backgroundColor);
+  const textHex = activeDiv.querySelector('h2');
+  const icons = activeDiv.querySelectorAll('.controls button');
+  textHex.innerText = color.hex();
+  //check contrast
+  checkTextContrast(color, textHex);
+  for (icon of icons) {
+    checkTextContrast(color, icon);
+  }
+}
+function resetInputs() {
+  const sliders = document.querySelectorAll('.sliders input');
+  sliders.forEach((slider) => {
+    if (slider.name === 'hue') {
+      const hueColor = initialColors[slider.getAttribute('data-hue')];
+      const hueValue = chroma(hueColor).hsl()[0];
+      slider.value = Math.floor(hueValue);
+      //this is where the break is in the code
+    }
+  });
+}
+function copyToClipboard(hex) {
+  const el = document.createElement('textarea');
+  el.value = hex.innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  //popup animation
+  const popupBox = popup.children[0];
+  popup.classList.add('active');
+  popupBox.classList.add('active');
+}
+
+randomColor();
