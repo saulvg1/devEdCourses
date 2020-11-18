e23c78af3a9276faca19b0ef6f07b57f173529ae
Saul Vega <saul.vg1@gmail.com>
2020-11-17 20:58:32 -0700

    restarting section

---
+++ creativeJavascriptCourse/websiteProject/app.js
@@ -0,0 +1,19 @@
+// let controller;
+// let scene;
+// function animateSlides() {
+//   //init controller
+//   controller = new ScrollMagic.Controller();
+//   //select somethings
+//   const sliders = document.querySelectorAll('.slide');
+//   const nav = document.querySelector('.nav-header');
+//   sliders.forEach((slide) => {
+//     const revealImg = slide.querySelector('.reveal-img');
+//     const img = slide.querySelector('img');
+//     const revealText = slide.querySelector('.reveal-text');
+//     //GSAP
+//     gsap.to(revealImg, 1, { x: '100%' });
+//     gsap.to(revealText, 1, { x: '100%' });
+//   });
+// }
+
+// animateSlides();
