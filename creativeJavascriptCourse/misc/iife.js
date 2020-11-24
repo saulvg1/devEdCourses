0bb6566bac7122e84ad7b2949e7bd52219b4386d
Saul Vega <saul.vg1@gmail.com>
2020-11-23 21:45:58 -0700

    finish IIFE

---
+++ creativeJavascriptCourse/misc/iife.js
@@ -0,0 +1,7 @@
+//IIFE
+// immediately invoked function Expression
+
+(function hello() {
+  console.log('hello, how are you?');
+})();
+// you can wrap the function in parenthesis and then follow with () to invoke it
