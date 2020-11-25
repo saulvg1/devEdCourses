b0693091cc25510117c636fb6fbe2b24d922195e
Saul Vega <saul.vg1@gmail.com>
2020-11-24 23:55:16 -0700

    finish refactor

--- creativeJavascriptCourse/photon/app.js
+++ creativeJavascriptCourse/photon/app.js
@@ -1,29 +1,51 @@
 const auth = '563492ad6f917000010000012f3ba0acb8d34764a847c4224b7dc0c4';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    'https://api.pexels.com/v1/curated?per_page=15',
-    {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: auth,
-      },
-    }
-  );
+//eventListeners
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(event) {
+  searchValue = event.target.value;
+}
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: auth,
+    },
+  });
   const data = await dataFetch.json();
-  console.log(data);
+  return data;
+}
+
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement('div');
     galleryImg.classList.add('gallery-img');
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-    <p>${photo.photographer}</p>
-    `;
+    galleryImg.innerHTML = `<img src=${photo.src.medium}></img>
+        <p>${photo.photographer}</p>
+        `;
     gallery.appendChild(galleryImg);
   });
+}
+
+async function curatedPhotos() {
+  const data = await fetchApi('https://api.pexels.com/v1/curated?per_page=15');
+  generatePictures(data);
+}
+
+async function searchPhotos(query) {
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15`
+  );
+  generatePictures(data);
 }
 curatedPhotos();
