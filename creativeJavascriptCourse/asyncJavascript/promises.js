58635e64e3d5cb78d1fc7d1da6e3f55e7680e4f2
Saul Vega <saul.vg1@gmail.com>
2020-11-24 22:03:19 -0700

    finish promises section

---
+++ creativeJavascriptCourse/asyncJavascript/promises.js
@@ -0,0 +1,14 @@
+//PROMISE
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('got the user');
+    // resolve({ user: 'Ed' });
+    reject(new Error('User not logged in'));
+  }, 2000);
+});
+
+promise
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => console.log(err.message));
