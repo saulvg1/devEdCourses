dd4c024837df08249060f713f819170fa33e2b11
Saul Vega <saul.vg1@gmail.com>
2020-11-25 00:32:03 -0700

    finish photon

--- creativeJavascriptCourse/photon/app.js
+++ creativeJavascriptCourse/photon/app.js
@@ -2,14 +2,21 @@ const auth = '563492ad6f917000010000012f3ba0acb8d34764a847c4224b7dc0c4';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
+const more = document.querySelector('.more');
 let searchValue;
+
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 //eventListeners
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+more.addEventListener('click', loadMore);
 
 function updateInput(event) {
   searchValue = event.target.value;
@@ -42,18 +49,28 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-  const data = await fetchApi('https://api.pexels.com/v1/curated?per_page=15');
+  fetchLink = 'https://api.pexels.com/v1/curated?per_page=15&page=1';
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(query) {
   clear();
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/search?query=${query}&per_page=15`
-  );
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 function clear() {
   gallery.innerHTML = '';
+}
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
 }
 curatedPhotos();
