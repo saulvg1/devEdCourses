89654032f73f72db9c172cd26aaec84dee2b1f7e
Saul Vega <saul.vg1@gmail.com>
2020-12-01 22:34:47 -0700

    finish class or className

---
+++ creativeReactAndReduxCourse/basics/react-starter/src/testComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <h3>From Test compoent</h3>
+    </div>
+  );
+}
+export default TestComponent;
