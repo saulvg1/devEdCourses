3900ecc1fdf9933175d7f443702136c93f642e6a
Saul Vega <saul.vg1@gmail.com>
2020-12-06 09:24:42 -0700

    finish inpute slider update

--- creativeReactAndReduxCourse/waveMusicPlayer/src/components/player.js
+++ creativeReactAndReduxCourse/waveMusicPlayer/src/components/player.js
@@ -29,6 +29,10 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
     );
   };
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
   //state
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
@@ -38,7 +42,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          type="range"
+          min="0"
+          value={songInfo.currentTime}
+          max={songInfo.duration}
+          onChange={dragHandler}
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
