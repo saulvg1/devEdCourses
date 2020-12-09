5eefb828a249b9adbc0767351aae8c482884dc9f
Saul Vega <saul.vg1@gmail.com>
2020-12-09 07:31:27 -0700

    finish 4: faq section

---
+++ creativeReactAndReduxCourse/capturePortfolio/src/components/faq.js
@@ -0,0 +1,62 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            quia.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Temporibus.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            quia.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Temporibus.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            quia.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Temporibus.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+            quia.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Temporibus.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default FaqSection;
--- creativeReactAndReduxCourse/capturePortfolio/src/pages/aboutUs.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/pages/aboutUs.js
@@ -1,5 +1,13 @@
 import AboutSection from '../components/aboutSection';
+import ServicesSection from '../components/servicesSection.js';
+import FaqSection from '../components/faq.js';
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 export default AboutUs;
