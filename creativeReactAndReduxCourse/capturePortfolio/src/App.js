34ddcf56bac598cdd5183064123edfbda04239e5
Saul Vega <saul.vg1@gmail.com>
2020-12-09 07:59:59 -0700

    finish 5: intro to styled components

--- creativeReactAndReduxCourse/capturePortfolio/src/App.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/App.js
@@ -1,7 +1,9 @@
 import AboutUs from './pages/aboutUs.js';
+import GlobalStyle from './components/globalStyle.js';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
