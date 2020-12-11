a911f64fb6a676e84bcdc21b9199e4feec83f460
Saul Vega <saul.vg1@gmail.com>
2020-12-11 00:44:50 -0700

    finish 8: creating our nav

--- creativeReactAndReduxCourse/capturePortfolio/src/App.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/App.js
@@ -1,9 +1,13 @@
+//component imports
+import Nav from './components/nav.js';
 import AboutUs from './pages/aboutUs.js';
+
 import GlobalStyle from './components/globalStyle.js';
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
