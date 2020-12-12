47d1574bb9bf8305ffe0f8192c346a3bfcad430d
Saul Vega <saul.vg1@gmail.com>
2020-12-12 15:58:18 -0700

    finish 11: movie details

--- creativeReactAndReduxCourse/capturePortfolio/src/App.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/App.js
@@ -5,6 +5,7 @@ import Nav from './components/nav.js';
 import AboutUs from './pages/aboutUs.js';
 import OurWork from './pages/ourWork.js';
 import ContactUs from './pages/contactUs.js';
+import MovieDetail from './pages/movieDetail.js';
 
 import GlobalStyle from './components/globalStyle.js';
 function App() {
@@ -20,7 +21,9 @@ function App() {
         <Route path="/work" exact>
           <OurWork />
         </Route>
-
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
         <Route path="/contact" exact>
           <ContactUs />
         </Route>
