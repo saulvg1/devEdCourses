5e3eb1134d8659fa4397e73d437502177192b9de
Saul Vega <saul.vg1@gmail.com>
2020-12-12 19:33:24 -0700

    finish 17: reusable compoenents

--- creativeReactAndReduxCourse/capturePortfolio/src/components/faqSection.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/components/faqSection.js
@@ -1,68 +1,79 @@
+import { useState } from 'react';
 //styled
 import styled from 'styled-components';
 import { About } from '../styles.js';
+import Toggle from './toggle.js';
 const FaqSection = () => {
+  const [faqToggle, setFaqToggle] = useState(false);
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            quia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Temporibus.
-          </p>
+      <Toggle>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              quia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Temporibus.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            quia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Temporibus.
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              quia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Temporibus.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            quia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Temporibus.
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              quia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Temporibus.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
-            quia.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Temporibus.
-          </p>
+      </Toggle>
+      <Toggle>
+        <div className="question">
+          <h4>What Products do you offer?</h4>
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+              quia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Temporibus.
+            </p>
+          </div>
+          <div className="faq-line"></div>
         </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
     </Faq>
   );
 };
