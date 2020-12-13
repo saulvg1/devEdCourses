d685d34f8f7257b81c0af24c79a5e0aaed16c15c
Saul Vega <saul.vg1@gmail.com>
2020-12-12 18:34:57 -0700

    finish 14: about animation

--- creativeReactAndReduxCourse/capturePortfolio/src/components/aboutSection.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/components/aboutSection.js
@@ -4,35 +4,34 @@ import home1 from '../img/home1.png';
 import { About, Description, Image, Hide } from '../styles.js';
 //framer motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation.js';
 
 const AboutSection = () => {
-  //framer motion variant
-
   return (
     <About>
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>Dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills
-        </p>
+        </motion.p>
 
-        <button>Contact Us</button>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </About>
   );
