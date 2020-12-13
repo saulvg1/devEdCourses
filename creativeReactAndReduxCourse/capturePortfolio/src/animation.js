4242906c39235cf92dbdfcd46d139e19db9f353c
Saul Vega <saul.vg1@gmail.com>
2020-12-12 19:17:07 -0700

    finish 16: our work animation

--- creativeReactAndReduxCourse/capturePortfolio/src/animation.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/animation.js
@@ -14,7 +14,7 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
+
     transition: {
       duration: 0.5,
     },
@@ -44,4 +44,21 @@ export const photoAnim = {
       duration: 0.75,
     },
   },
+};
+export const lineAnim = {
+  hidden: { width: '0%' },
+  show: { width: '100%', transition: { duration: 1 } },
+};
+
+export const slider = {
+  hidden: { x: '-130%', skew: '55deg' },
+  show: {
+    x: '100%',
+    skew: '0deg',
+    transition: { ease: 'easeOut', duration: 1 },
+  },
+};
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: 'easeOut' } },
 };
--- creativeReactAndReduxCourse/capturePortfolio/src/pages/ourWork.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/pages/ourWork.js
@@ -2,7 +2,15 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 //framer motion
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation.js';
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from '../animation.js';
+
 //images
 import athlete from '../img/athlete-small.png';
 import theracer from '../img/theracer-small.png';
@@ -17,25 +25,38 @@ const OurWork = () => {
       exit="exit"
       style={{ background: '#fff' }}
     >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
+
       <Movie>
-        <h2>the Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>the Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="athlete" />
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Racer</motion.h2>
+        <motion.div className="line"></motion.div>
         <Link to="/work/the-racer">
-          <img src={theracer} alt="theracer" />
+          <Hide>
+            <motion.img variants={photoAnim} src={theracer} alt="theracer" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>Good Times</motion.h2>
+        <motion.div className="line"></motion.div>
         <Link to="/work/good-times">
-          <img src={goodtimes} alt="goodtimes" />
+          <Hide>
+            <motion.img variants={photoAnim} src={goodtimes} alt="goodtimes" />
+          </Hide>
         </Link>
       </Movie>
     </Work>
@@ -54,7 +75,7 @@ const Movie = styled(motion.div)`
   /* color: white; */
   .line {
     height: 0.5rem;
-    background: #ccc;
+    background: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -62,5 +83,27 @@ const Movie = styled(motion.div)`
     height: 70vh;
     object-fit: cover;
   }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+//Frame animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 export default OurWork;
