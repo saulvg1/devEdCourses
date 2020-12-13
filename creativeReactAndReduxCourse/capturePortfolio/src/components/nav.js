36ccbfa50fe686d1dac6f7d276f763c5105ffcb6
Saul Vega <saul.vg1@gmail.com>
2020-12-12 22:34:23 -0700

    finish 23: mobile friendly

--- creativeReactAndReduxCourse/capturePortfolio/src/components/nav.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/components/nav.js
@@ -33,6 +33,7 @@ const StyledNav = styled.nav`
   position: sticky;
   top: 0;
   z-index: 10;
+
   a {
     color: white;
     text-decoration: none;
@@ -49,6 +50,22 @@ const StyledNav = styled.nav`
   li {
     padding-left: 10rem;
     position: relative;
+  }
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    padding: 2rem 1rem;
+    #logo {
+      display: inline-block;
+      margin: 1rem;
+    }
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+      li {
+        padding: 0;
+      }
+    }
   }
 `;
 export default Nav;
--- creativeReactAndReduxCourse/capturePortfolio/src/components/useScroll.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/components/useScroll.js
@@ -2,7 +2,7 @@ import { useInView } from 'react-intersection-observer';
 import { useAnimation } from 'framer-motion';
 export const useScroll = () => {
   const controls = useAnimation();
-  const [element, view] = useInView({ threshold: 0.5 });
+  const [element, view] = useInView({ threshold: 0.3 });
   if (view) {
     controls.start('show');
   } else {
