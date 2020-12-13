1d7f1014c5a08670a625e6d44a1dc51112bae813
Saul Vega <saul.vg1@gmail.com>
2020-12-12 21:39:21 -0700

    finish 22: contact animation

--- creativeReactAndReduxCourse/capturePortfolio/src/pages/contactUs.js
+++ creativeReactAndReduxCourse/capturePortfolio/src/pages/contactUs.js
@@ -1,19 +1,68 @@
 //framer motion
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation.js';
+import { pageAnimation, titleAnim } from '../animation.js';
+import styled from 'styled-components';
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
       style={{ background: '#fff' }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in Touch</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send Us a Message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send Us an Email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Social</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
-
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background-color: #353535;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 export default ContactUs;
